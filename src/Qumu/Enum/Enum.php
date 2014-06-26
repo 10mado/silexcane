becded93a0f243934480ea05da15dcbabde567c5
Hiroyuki Yamaoka <yamaoka@qumu.co>
2014-06-26 22:55:54 +0900

    Correct self to using static

--- src/Qumu/Enum/Enum.php
+++ src/Qumu/Enum/Enum.php
@@ -5,17 +5,22 @@ abstract class Enum
 {
     protected static $data = [];
 
+    public static function all()
+    {
+        return static::$data;
+    }
+
     public static function getName($code)
     {
-        if (array_key_exists($code, self::$data)) {
-            return self::$data[$code];
+        if (array_key_exists($code, static::$data)) {
+            return static::$data[$code];
         }
         return null;
     }
 
     public static function getCode($name)
     {
-        $code = array_search($name, self::$data);
+        $code = array_search($name, static::$data);
         if ($code !== false) {
             return $code;
         }
