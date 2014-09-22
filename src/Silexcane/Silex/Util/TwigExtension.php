083c119646bbea225b4011120b4046b06b241078
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2014-09-22 10:51:03 +0900

    Add TwigExtension

---
+++ src/Silexcane/Silex/Util/TwigExtension.php
@@ -0,0 +1,18 @@
+<?php
+namespace Silexcane\Silex\Util;
+
+use Silexcane\Silex\Application;
+
+class TwigExtension
+{
+    public static function extend(Application $app)
+    {
+        $app['twig']->addFilter(new \Twig_SimpleFilter('autolink', function($string) {
+            return preg_replace(
+                '/((http|https|ftp):\/\/[\w?=&.\/-;#~%\-]+(?![\w\s?&.\/;#~%"=\-]*>))/',
+                '<a href="$1" target="_blank" rel="nofollow">$1</a>',
+                $string
+            );
+        }, ['is_safe' => ['html']]));
+    }
+}
