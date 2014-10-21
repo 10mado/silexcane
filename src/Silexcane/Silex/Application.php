6d501717125936992aca97b73361ee954ed73d9f
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2014-10-21 10:31:34 +0900

    Add Application::xml

--- src/Silexcane/Silex/Application.php
+++ src/Silexcane/Silex/Application.php
@@ -5,4 +5,11 @@ class Application extends \Silex\Application
 {
     use \Silex\Application\TwigTrait;
     use \Silex\Application\UrlGeneratorTrait;
+
+    public function xml($template, array $values = [])
+    {
+        $response = $this->render($template, $values);
+        $response->headers->set('Content-Type', 'application/xml');
+        return $response;
+    }
 }
